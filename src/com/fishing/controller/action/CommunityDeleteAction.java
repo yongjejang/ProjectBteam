224da4e83d2557735b9dd1e4e30f5176323292bb
pkt <kkx00000@gmail.com>
2018-08-07 14:05:34 +0900

    삭제 글 확인버튼 수정

--- src/com/fishing/controller/action/CommunityDeleteAction.java
+++ src/com/fishing/controller/action/CommunityDeleteAction.java
@@ -1,6 +1,8 @@
 package com.fishing.controller.action;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -21,6 +23,11 @@ public class CommunityDeleteAction implements Action {
 		System.out.println("넘값 확인" + communityNum);
 		CommunityDAO cdao = CommunityDAO.getInstance();
 		cdao.deleteBoard(communityNum);
+		PrintWriter w = response.getWriter();
+		
+		
+		
+		
 		
 		new CommunityListAction().excute(request, response);
 		
